1a694674b352f0b4e94892d373c070b1d3840e02
trance <trance@example.com>
2026-06-20 23:51:22 +0800

    fix

--- app/src/main/java/com/huanchengfly/tieba/post/ui/page/main/home/HomePage.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/page/main/home/HomePage.kt
@@ -103,9 +103,9 @@ import com.huanchengfly.tieba.post.ui.widgets.compose.rememberMenuState
 import com.huanchengfly.tieba.post.ui.widgets.compose.states.StateScreen
 import com.huanchengfly.tieba.post.utils.AccountUtil.LocalAccount
 import com.huanchengfly.tieba.post.utils.ImageUtil
-import com.huanchengfly.tieba.post.utils.StringUtil
 import com.huanchengfly.tieba.post.utils.StringUtil.getShortNumString
 import com.huanchengfly.tieba.post.utils.TiebaUtil
+import com.huanchengfly.tieba.post.utils.LocalForumManager
 import com.huanchengfly.tieba.post.utils.appPreferences
 import kotlinx.collections.immutable.persistentListOf
 
